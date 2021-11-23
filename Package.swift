684b8afdb7d72c110299bd333bcc541ecccb7660
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-11-23 15:16:25 +0100

    Update SwiftRex to use Middleware IO

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "StateMachineMiddleware", targets: ["StateMachineMiddleware"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftRex/SwiftRex.git", .branch("MiddlewareRefactorSecondAttempt"))
+        .package(url: "https://github.com/SwiftRex/SwiftRex.git", .upToNextMajor(from: "0.8.8"))
     ],
     targets: [
         .target(name: "StateMachineMiddleware", dependencies: [.product(name: "CombineRex", package: "SwiftRex")])
